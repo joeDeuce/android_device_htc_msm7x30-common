cac629fefda357048553e466823c7051d7926ed4
Davor Bertovic <davor@losinj.com>
2012-01-15 21:57:43 +0100

    remove bugmailer, add Trebuchet to build

--- msm7x30.mk
+++ msm7x30.mk
@@ -79,7 +79,8 @@ PRODUCT_PACKAGES += \
     FileManager \
     DSPManager \
     libcyanogen-dsp \
-    Torch
+    Torch \
+    Trebuchet
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
@@ -98,14 +99,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
-
-# for bugmailer
-ifneq ($(TARGET_BUILD_VARIANT),user)
-    PRODUCT_PACKAGES += send_bug
-    PRODUCT_COPY_FILES += \
-        system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
-        system/extras/bugmailer/send_bug:system/bin/send_bug
-endif
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
